ba4abad94b7e2d338ec4f72adeb980d3505bc27b
MICHAEL STEOGER <ikesteoger@gmail.com>
2023-05-02 11:26:22 -0700

    fixing up routes

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navbar, Nav, Dropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom'
 
 class Header extends React.Component {
   render() {
@@ -10,8 +11,7 @@ class Header extends React.Component {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="./about">About the Team</Nav.Link>
-
+              <Link to="/about">About the Team</Link>
             </Nav>
             <Nav>
               <Dropdown drop='down-centered' align={{md:'end'}}>
