9bb94e88564bf4e7596c4a70ed41a634e555b39e
KaedenOC <komeacarr2@gmail.com>
2023-05-02 12:05:46 -0700

    add modal form and adding resource

--- src/components/resources.js
+++ src/components/resources.js
@@ -4,12 +4,18 @@ import Accordion from 'react-bootstrap/Accordion';
 import AccordionHeader from 'react-bootstrap/esm/AccordionHeader';
 import AccordionItem from 'react-bootstrap/esm/AccordionItem';
 import AccordionBody from 'react-bootstrap/esm/AccordionBody';
+import Button from 'react-bootstrap/Button';
+import ResourceModal from './resourceModal';
 
 class Resources extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             resources: [],
+            title: '',
+            description: '',
+            url: '',
+            showModal: false
         }
     }
 
@@ -26,9 +32,50 @@ class Resources extends React.Component {
         }
     }
 
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        let resourceObj = {
+            title: e.target.resTitle.value,
+            description: e.target.resDesc.value,
+            url: e.target.resUrl.value,
+        }
+        this.postResource(resourceObj);
+        // console.log(`bookObj`);
+    }
+
+    handleModalShow = () => {
+        this.setState({
+            showModal: true
+        })
+    }
+
+    handleModalClose = () => {
+        this.setState({
+            showModal: false
+        })
+    }
+
+    postResource = async (resourceObj) => {
+        try {
+            let url = `${process.env.REACT_APP_SERVER}/resources`;
+
+            let postResource = await axios.post(url, resourceObj);
+
+            this.setState({
+                resources: [...this.state.resources, postResource.data]
+            })
+
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
     componentDidMount() {
         this.getResources();
     }
+
+
 
     render() {
         return (
@@ -42,11 +89,17 @@ class Resources extends React.Component {
                                     <a href={resource.url} target="_blank" rel="noopener noreferrer">{resource.url}</a>
                                 </li>
                             ))}
+                            <Button onClick={() => this.handleModalShow()}>Add Resource </Button>
                         </AccordionBody>
                     </AccordionItem>
 
 
                 </Accordion>
+                <ResourceModal
+                    showModal={this.state.showModal}
+                    handleModalClose={() => this.handleModalClose()}
+                    handleSubmit={this.handleSubmit}
+                />
             </>
         )
     }
