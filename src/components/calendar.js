cac24db8fd8a18c15eee496d6151c2ffe381af3a
MICHAEL STEOGER <ikesteoger@gmail.com>
2023-05-02 15:19:21 -0700

    calendar start

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -1,10 +1,50 @@
 import React from 'react';
+import axios from 'axios';
 
 class Calendar extends React.Component {
-    render(){
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            calendar: [],
+            showCalendar: false,
+        }
+    }
+
+    getCalendar = async (request, response, next) => {
+        try {
+            let url = `${process.env.REACT_APP_SERVER}/calendar`
+            let calendarData = await axios.get(url)
+            console.log(calendarData.data)
+            this.setState({
+                calendar: calendarData.data,
+                showCalendar: true,
+            })
+
+            // response.status(200).send(memeData.data)
+        } catch (error) {
+            next(error)
+
+            this.setState({
+                showCalendar: false,
+                calendar: [],
+            })
+        }
+    }
+
+    componentDidMount() {
+        this.getCalendar();
+      }
+
+      render(){
         return(
             <>
-                <p>Calendar soon!</p>
+            <ul>
+            {
+                this.state.calendar.map(taskInfo => 
+                   <li>{taskInfo.task} due on {Date(taskInfo.dueDate)}</li>)
+            }
+            </ul>
             </>
         )
     }
