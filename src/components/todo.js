a9153643f511715ae61b8e11327e1629dc1119c2
Donna Ada <125130284+donnaada@users.noreply.github.com>
2023-05-03 09:51:15 +1000

    adding add and delete function

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import TaskProgress from './todo-progressbar';
+import { ListGroup,Button, ButtonGroup } from 'react-bootstrap';
+import AddTask from './addTask';
+import { PencilSquare, Trash3 } from 'react-bootstrap-icons';
 
 const SERVER = process.env.REACT_APP_SERVER;
+
+
 
 class ToDo extends Component {
   constructor(props) {
@@ -10,26 +15,60 @@ class ToDo extends Component {
     this.state = {
       todoList: [],
       taskCompleted: 0,
-      totalTasks: 0
+      totalTasks: 0,
+      isEditing: false,
+      taskId: '',
+      readOnly: true
     }
   }
-
+  
   getTodoList = async (req, res, next) => {
-    try {
       let url = `${SERVER}/todo`;
-      let todoListData = await axios.get(url);
+      let dataFromAPI = await axios.get(url);
+      let todoTasks = dataFromAPI.data;
+
 
       this.setState({
-        todoList: todoListData.data,
-        totalTasks: todoListData.data.length
+        todoList: todoTasks,
+        totalTasks: todoTasks.length
       })
+  }
 
-      res.status(200).send(todoListData)
-
+  addTodoTask = async (task, next) => {
+    try {
+      let url = `${SERVER}/todo`;
+      await axios.post(url, task);
+      this.getTodoList();
     } catch (error) {
       console.log(error)
     }
   }
+  
+  deleteTodoTask = async (taskToDelete)=>{
+    let url = `${SERVER}/todo/${taskToDelete}`;
+    try {
+      await axios.delete(url);
+      
+      let updatedList = this.state.todoList.filter(item => item._id !== taskToDelete._id);
+      console.log('Task Deleted Succesfully')
+      
+      this.setState({ 
+        todoList: updatedList 
+      });
+
+      this.getTodoList();
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  handleEdit = (itemId) => {
+    this.setState({
+      isEditing: true,
+      taskId: itemId,
+      readOnly: false
+    });
+  };
 
   componentDidMount() {
     this.getTodoList();
@@ -39,20 +78,28 @@ class ToDo extends Component {
   render() {
     return (
       <>
+      <AddTask handleAddTask={this.addTodoTask}/>
         <TaskProgress totalTasks={this.state.totalTasks} />
-        {this.state.todoList.map(item => {
-          return (
-            <>
-              <details key={item._id}>
-                <summary>
-                  {item.task}
-                </summary>
-                <p>{item.dueDate}</p>
-                <p>{item.completed}</p>
-              </details>
-            </>
-          )
-        })}
+        <ListGroup>
+          {this.state.todoList.map(item => {
+            return (
+              <ListGroup.Item key={item._id}>
+                  <div className='d-flex align-item-center justify-content-between'>
+                    <div>
+                      <input className="form-check-input me-1" type="checkbox" value={item.completed} />
+                      <label className='form-check-label'>{item.task}</label>
+                    </div>
+                    <ButtonGroup>
+                      <Button variant="outline-secondary" size="sm"><PencilSquare/></Button>
+                      <Button variant="outline-danger" size="sm" onClick={()=> this.deleteTodoTask(item._id)}><Trash3/></Button>
+                    </ButtonGroup>
+                  </div>
+                  <p className='text-start text-secondary fs'>Due {item.dueDate}</p>
+              </ListGroup.Item>
+
+            )
+          })}
+        </ListGroup>
       </>
     )
   }
