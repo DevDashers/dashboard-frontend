8c845de1c7f9047b3d8a7d3f968a96cc40f4e1f1
Heather Holcomb <holcombheather@gmail.com>
2023-05-01 20:55:12 -0700

    Auth0 login quick start install & verified functionality for front end

--- src/App.js
+++ src/App.js
@@ -2,17 +2,30 @@ import React from 'react';
 import Header from './components/header';
 import Main from './components/main';
 import Footer from './components/footer';
+import LoginButton from './components/Login';
+import LogoutButton from './components/Logout';
+import Profile from './components/Profile';
+import { withAuth0 } from "@auth0/auth0-react";
 
 class App extends React.Component {
     render(){
         return(
             <>
-            <Header />
-            <Main />
-            <Footer />
+                <Header />
+                    {
+                        this.props.auth0.isAuthenticated ?
+                            <>
+                                <Profile />
+                                <LogoutButton />
+                                <Main />
+                            </>
+                        :                     
+                    <LoginButton />
+                }
+                    <Footer />  
             </>
         )
     }
 }
 
-export default App;
+export default withAuth0(App);
