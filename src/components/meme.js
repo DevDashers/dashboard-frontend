74a80cddf00c87954a1536edbad9e3b280e4a54a
MICHAEL STEOGER <ikesteoger@gmail.com>
2023-05-02 13:13:25 -0700

    fixing about us links & small changes to meme component

--- src/components/meme.js
+++ src/components/meme.js
@@ -5,12 +5,12 @@ class Meme extends React.Component {
     render() {
         return (
             <div className='d-flex flex-column justify-content-center'>
+                <Button className='mx-2' onClick={() => this.props.getMeme()}>Click to smile!</Button>
                 {this.props.showMeme
-                    ? <><p>{this.props.currentMeme[0]} created by {this.props.currentMeme[1]}</p>
-                        <Image className='mx-auto' src={this.props.currentMeme[2]} alt="" width={250} fluid/></>
+                    ? <><Image className='mx-auto' src={this.props.currentMeme[2]} alt="" width={250} fluid/>
+                    <p>{this.props.currentMeme[0]} created by {this.props.currentMeme[1]}</p></>
                     : <></>
                 }
-                <Button className='mx-2' onClick={() => this.props.getMeme()}>Click to smile!</Button>
             </div>
         )
     }
