e58431bdb8862994a8c51a3641e857a9fa5457da
MICHAEL STEOGER <ikesteoger@gmail.com>
2023-05-02 18:04:39 -0700

    swapped toDo state to main

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,87 +1,18 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import TaskProgress from './todo-progressbar';
 import { ListGroup,Button, ButtonGroup } from 'react-bootstrap';
 import AddTask from './addTask';
 import { PencilSquare, Trash3 } from 'react-bootstrap-icons';
 
-const SERVER = process.env.REACT_APP_SERVER;
-
-
-
 class ToDo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todoList: [],
-      taskCompleted: 0,
-      totalTasks: 0,
-      isEditing: false,
-      taskId: '',
-      readOnly: true
-    }
-  }
   
-  getTodoList = async (req, res, next) => {
-      let url = `${SERVER}/todo`;
-      let dataFromAPI = await axios.get(url);
-      let todoTasks = dataFromAPI.data;
-
-
-      this.setState({
-        todoList: todoTasks,
-        totalTasks: todoTasks.length
-      })
-  }
-
-  addTodoTask = async (task, next) => {
-    try {
-      let url = `${SERVER}/todo`;
-      await axios.post(url, task);
-      this.getTodoList();
-    } catch (error) {
-      console.log(error)
-    }
-  }
-  
-  deleteTodoTask = async (taskToDelete)=>{
-    let url = `${SERVER}/todo/${taskToDelete}`;
-    try {
-      await axios.delete(url);
-      
-      let updatedList = this.state.todoList.filter(item => item._id !== taskToDelete._id);
-      console.log('Task Deleted Succesfully')
-      
-      this.setState({ 
-        todoList: updatedList 
-      });
-
-      this.getTodoList();
-    } catch (error) {
-      console.log(error.message)
-    }
-  }
-
-  handleEdit = (itemId) => {
-    this.setState({
-      isEditing: true,
-      taskId: itemId,
-      readOnly: false
-    });
-  };
-
-  componentDidMount() {
-    this.getTodoList();
-  }
-
-
   render() {
     return (
       <>
-      <AddTask handleAddTask={this.addTodoTask}/>
-        <TaskProgress totalTasks={this.state.totalTasks} />
+      <AddTask handleAddTask={this.props.addTodoTask} />
+        <TaskProgress totalTasks={this.props.totalTasks} />
         <ListGroup>
-          {this.state.todoList.map(item => {
+          {this.props.todoList.map(item => {
             return (
               <ListGroup.Item key={item._id}>
                   <div className='d-flex align-item-center justify-content-between'>
@@ -91,7 +22,7 @@ class ToDo extends Component {
                     </div>
                     <ButtonGroup>
                       <Button variant="outline-secondary" size="sm"><PencilSquare/></Button>
-                      <Button variant="outline-danger" size="sm" onClick={()=> this.deleteTodoTask(item._id)}><Trash3/></Button>
+                      <Button variant="outline-danger" size="sm" onClick={()=> this.props.deleteTodoTask(item._id)}><Trash3/></Button>
                     </ButtonGroup>
                   </div>
                   <p className='text-start text-secondary fs'>Due {item.dueDate}</p>
