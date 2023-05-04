8e331d3830de9c5a0c1ecb4d928d491d44287bcc
Heather Holcomb <holcombheather@gmail.com>
2023-05-04 11:23:55 -0700

    splash styling

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,15 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 
+
+const buttonStyle = {
+  color: "white"
+}
+
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return <button id="button1" onClick={() => loginWithRedirect()}>Log In</button>;
 };
 
 export default LoginButton;
-
-//Auth0 template
--- src/components/splash.js
+++ src/components/splash.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import LoginButton from './Login';
+import '../splash.css';
+import VideoBackground from  '../assets/retroSplashVideo1.mp4';
+import '../index.css';
+
 
 class Splash extends React.Component {
+ 
+
+
     render(){
         return(
-            <>
-                <p>Splash soon!</p>
-            </>
-        )
+            <container d-flex align-items-center justify-content-center text-center h-100>
+                <div id="splashDiv" >
+                    <div className="overlay"></div>
+                        <video src={VideoBackground} autoPlay loop muted />
+                        <div className="content">
+                            <h1 id="h1splash">Dev Dashers</h1>                
+                            <LoginButton />
+                        </div>
+
+            </div> 
+            </container>
+
+
+        );
     }
 }
 
